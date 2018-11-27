f1d6305326d837f78de77ef1471eaa0ccb754307
ILope <aiswandev@outlook.com>
2018-11-27 19:57:01 +0300

    Add __slots__, add method 'memory', delete __init__, memory not add in instance object

--- size_object.py
+++ size_object.py
@@ -1,22 +1,30 @@
 from pympler import asizeof
 
 
-class ObjectMemory:
-    def __init__(self, obj, show=False):
-        self.__memory = 0
-        self.__in = True
-        self.__show = show
-
+class Memory:
+    __slots__ = ('__memory', '__in', '__show')
     def __counter_size(self, obj):
         size = asizeof.asizeof(obj)
         if self.__show is True:
             print(size, 'bytes in:', obj)
         return size
 
-    def memory_null(self):
+    def memory(self, obj, show=False):
         self.__memory = 0
+        self.__in = True
+        self.__show = show
+        return self.__size_obj(obj)
 
-    def size_obj(self, obj):
+    def __size_obj(self, obj):
+        """Iter obj
+        
+        Arguments:
+            obj {[all]} -- [object memory]
+        
+        Returns:
+            [int] -- [memory in obj]
+        """
+
         if self.__in:
             if type(obj) is tuple:
                 self.__memory += self.__counter_size(())
@@ -30,12 +38,19 @@ class ObjectMemory:
         for i in obj:
             if type(i) is tuple:
                 self.__memory += self.__counter_size(())
-                self.size_obj(i)
+                self.__size_obj(i)
             elif type(i) is list:
                 self.__memory += self.__counter_size([])
-                self.size_obj(i)
+                self.__size_obj(i)
             else:
                 cnt = self.__counter_size(i)
                 self.__memory += cnt
 
         return self.__memory
+
+
+test_data_1 = (0, 1, [100, 10, 100, (99, 98, 97)], 1000)
+m = Memory()
+
+print('Real:', m.memory(test_data_1))
+# Real: 440
